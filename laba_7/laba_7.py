718178887d708963b01ed89e8f5ce6e6da5e81a5
bagg1487 <kutenand2@gmail.com>
2024-10-21 09:08:08 +0700

    Update laba_7.py

--- laba_7/laba_7.py
+++ laba_7/laba_7.py
@@ -1,9 +1,9 @@
 '''# 1
-sentence=input()
-words=sentence.split()
+words=input().split()
 for i in range(len(words)-1):
-    if words[i]==words[i+1]:
+    if words[i] != words[i+1]:
         print(words[i])
+print(words[-1])
 '''
 '''# 2
 sentence=input()
